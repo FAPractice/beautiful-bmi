b0540d56926b2279f6989757b31ecae1e2e7403b
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2021-10-10 22:47:33 +0530

    Initial Commit

---
+++ Shared/Beautiful_BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beautiful_BMIApp.swift
+//  Shared
+//
+//  Created by Harsh Chaturvedi on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Beautiful_BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
